51bc4c4e6f24a7b929b0cf0729a792a2c04148b3
hamishgibbs <hamishgibbs@gmail.com>
2021-10-27 07:52:03 +0100

    docs & clarity

--- mkgen/makefile.py
+++ mkgen/makefile.py
@@ -8,21 +8,27 @@ def construct_target(file, fns, io, interpreter):
 
     target = "\n" + name + ": " + " ".join(outputs) + "\n\n"
 
-    target = target + " ".join(outputs) + ": " + file + " \ \n\t\t" + " \ \n\t\t".join(inputs) + "\n\t" + interpreter + "\n\n"
+    target = target + " ".join(outputs)
+    target = target + ": " + file + " \\ \n\t\t" + " \\ \n\t\t".join(inputs)
+    target = target + "\n\t" + interpreter + "\n\n"
 
     return target
 
 
 def get_mkgen_indices(make_lines):
 
-    start = [i for i, x in enumerate(make_lines) if x == "# -- mkgen targets start --\n"][0]
-    end = [i for i, x in enumerate(make_lines) if x == "# -- mkgen targets end --\n"][0]
+    start_comment = "# -- mkgen targets start --\n"
+    end_comment = "# -- mkgen targets end --\n"
+
+    start = [i for i, x in enumerate(make_lines) if x == start_comment][0]
+    end = [i for i, x in enumerate(make_lines) if x == end_comment][0]
 
     return (start, end)
 
 
 def insert_new_targets(start, end, make_lines, targets):
-    # remove lines between the auto generated annotations
-    # and insert new targets between start and end indices
+    """
+    Insert new targets in Makefile between mkgen annotations
+    """
 
     return make_lines[:start+1] + targets + make_lines[end:]
